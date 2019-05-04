3e4302d440f0943416e4b75546a03819cfc533f5
cheney.yan <cheney.yan@gmail.com>
2019-05-04 23:51:57 +1000

    Fix it all

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tag=$(git tag | grep -e '^v' | tail -1)
-docker build -t cheneyyan/awslogin:$tag .
+docker build -t cheneyyan/awslogin:$tag . || exit 1
 docker push cheneyyan/awslogin:$tag
 docker tag cheneyyan/awslogin:$tag cheneyyan/awslogin:latest
 docker push cheneyyan/awslogin:latest
